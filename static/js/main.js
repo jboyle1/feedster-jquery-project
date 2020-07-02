985048f5d28c8e5d0a082a53813a6f08e5d8d36c
Jed Boyle <jedboyle01@gmail.com>
2020-07-02 11:43:11 +0100

    Add an event handler to main.js that hides the .nav-menu element when a user’s mouse leaves the .nav-menu element.

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(() => {
     $('.menu').on('mouseenter', () => {
         $('.nav-menu').show();
+    });
+
+    $('.nav-menu').on('mouseleave', () => {
+        $('.nav-menu').hide();
     })
 });
